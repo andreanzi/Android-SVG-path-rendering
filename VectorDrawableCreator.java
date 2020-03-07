7727bf88cee77128ce3cca0f7a746929437d6591
andreanzi <a.anzilotti@hastega.it>
2020-03-07 20:24:08 +0100

    Update VectorDrawableCreator.java
    Fixed code - now the supports strokeColor and strokeWidth attributes

--- VectorDrawableCreator.java
+++ VectorDrawableCreator.java
@@ -1,3 +1,5 @@
+package it.gruppometa.hubscuola.views;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
@@ -9,12 +11,15 @@ import android.util.Log;
 import android.util.Xml;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
@@ -28,17 +33,23 @@ import java.util.List;
 public class VectorDrawableCreator {
 
     private static final byte[][] BIN_XML_STRINGS = {
-            "width".getBytes(),
             "height".getBytes(),
+            "width".getBytes(),
             "viewportWidth".getBytes(),
             "viewportHeight".getBytes(),
             "fillColor".getBytes(),
             "pathData".getBytes(),
-            "strokeWidth".getBytes(),
             "strokeColor".getBytes(),
+            "strokeWidth".getBytes(),
+            "translateX".getBytes(),
+            "translateY".getBytes(),
+            "android".getBytes(),
+            "http://schemas.android.com/apk/res/android".getBytes(),
             "path".getBytes(),
             "vector".getBytes(),
-            "http://schemas.android.com/apk/res/android".getBytes()
+            "group".getBytes()
+
+
     };
 
     private static final int[] BIN_XML_ATTRS = {
@@ -48,19 +59,22 @@ public class VectorDrawableCreator {
             android.R.attr.viewportHeight,
             android.R.attr.fillColor,
             android.R.attr.pathData,
-            android.R.attr.strokeWidth,
-            android.R.attr.strokeColor
+            android.R.attr.strokeColor,
+            android.R.attr.strokeWidth
     };
 
     private static final short CHUNK_TYPE_XML = 0x0003;
     private static final short CHUNK_TYPE_STR_POOL = 0x0001;
     private static final short CHUNK_TYPE_START_TAG = 0x0102;
+    private static final short CHUNK_TYPE_NS_START_TAG = 0x0100;
     private static final short CHUNK_TYPE_END_TAG = 0x0103;
+    private static final short CHUNK_TYPE_NS_END_TAG = 0x0101;
     private static final short CHUNK_TYPE_RES_MAP = 0x0180;
 
     private static final short VALUE_TYPE_DIMENSION = 0x0500;
     private static final short VALUE_TYPE_STRING = 0x0300;
-    private static final short VALUE_TYPE_COLOR = 0x1C00;
+    private static final short VALUE_TYPE_COLOR_F = 0x1F00;
+    private static final short VALUE_TYPE_COLOR_D = 0x1D00;
     private static final short VALUE_TYPE_FLOAT = 0x0400;
 
 
@@ -87,12 +101,23 @@ public class VectorDrawableCreator {
             Class<?> xmlBlock = Class.forName("android.content.res.XmlBlock");
             Constructor xmlBlockConstr = xmlBlock.getConstructor(byte[].class);
             Method xmlParserNew = xmlBlock.getDeclaredMethod("newParser");
+            //InputStream is = context.getResources().openRawResource(context.getResources().getIdentifier("test2", "drawable", context.getPackageName()));
+            byte[] byteArray = new byte[8192];
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            int nRead = 0;
+//            while ((nRead = is.read(byteArray, 0, byteArray.length)) != -1) {
+//                buffer.write(byteArray, 0, nRead);
+//            }
             xmlBlockConstr.setAccessible(true);
             xmlParserNew.setAccessible(true);
+            byte[] test = buffer.toByteArray();
+            //logDifferences(test, binXml);
             logData((XmlPullParser) xmlParserNew.invoke(xmlBlockConstr.newInstance((Object) binXml)));
+            //logData((XmlPullParser) xmlParserNew.invoke(xmlBlockConstr.newInstance((Object) buffer.toByteArray())));
             XmlPullParser parser = (XmlPullParser) xmlParserNew.invoke(
                     xmlBlockConstr.newInstance((Object) binXml));
-            if (Build.VERSION.SDK_INT >= 24) {
+            // TODO: fix that check for Android < API 24
+            if (true) {
                 return Drawable.createFromXml(context.getResources(), parser);
             } else {
                 // Before API 24, vector drawables aren't rendered correctly without compat lib
@@ -111,6 +136,22 @@ public class VectorDrawableCreator {
             Log.e(VectorDrawableCreator.class.getSimpleName(), "Vector creation failed", e);
         }
         return null;
+    }
+
+    private static void logDifferences(byte[] arr1, byte[] arr2){
+        int size = arr1.length > arr2.length ? arr2.length : arr1.length;
+        int size2 = arr1.length > arr2.length ? arr1.length : arr2.length;
+        int k=0;
+        for (int i = 0; i<size; i++){
+            if(arr1[i] != arr2[i]){
+                Log.d("BYTEDIFF", i + " : "+String.format("%02X ", arr1[i])+ "  |  "+String.format("%02X ", arr2[i]));
+            }
+            k = i;
+        }
+        for (int j = k; j<size2; j++){
+            Log.d("BYTEDIFF", j + " : "+String.format("%02X ", arr1[j]));
+        }
+
     }
 
     private static void logData(XmlPullParser xpp) throws XmlPullParserException, IOException {
@@ -138,9 +179,10 @@ public class VectorDrawableCreator {
                                                   boolean stroke, int strokeColor, float translateX,
                                                   float translateY, List<PathData> paths) {
         List<byte[]> stringPool = new ArrayList<>(Arrays.asList(BIN_XML_STRINGS));
-        for (PathData path : paths) {
-            stringPool.add(path.data);
-        }
+        stringPool.add(12, paths.get(0).data);
+//        for (PathData path : paths) {
+//            stringPool.add(path.data);
+//        }
         //stringPool.add("@color/red".getBytes());
 
         ByteBuffer bb = ByteBuffer.allocate(8192);  // Capacity might have to be greater.
@@ -218,15 +260,20 @@ public class VectorDrawableCreator {
         }
 
         // ==== Vector start tag ====
+        putNsStartTag(bb, 11);
+
         int vstStartPos = bb.position();
-        int vstSizePos = putStartTag(bb, 9, 4);
+        int vstSizePos = putStartTag(bb, 14, 4, 1);
 
         // Attributes
-        // android:width="24dp", value type: dimension (dp)
-        putAttribute(bb, 0, -1, VALUE_TYPE_DIMENSION, (width << 8) + 1);
 
         // android:height="24dp", value type: dimension (dp)
-        putAttribute(bb, 1, -1, VALUE_TYPE_DIMENSION, (height << 8) + 1);
+        putAttribute(bb, 0, -1, VALUE_TYPE_DIMENSION, (height << 8) + 1);
+
+        // android:width="24dp", value type: dimension (dp)
+        putAttribute(bb, 1, -1, VALUE_TYPE_DIMENSION, (width << 8) + 1);
+
+
 
         // android:viewportWidth="24", value type: float
         putAttribute(bb, 2, -1, VALUE_TYPE_FLOAT, Float.floatToRawIntBits(viewportWidth));
@@ -239,34 +286,37 @@ public class VectorDrawableCreator {
         bb.putInt(vstSizePos, bb.position() - vstStartPos);
         bb.position(posBefore);
 
-        // ==== Group start tag ====
-//        int gstStartPos = bb.position();
-//        int gstSizePos = putStartTag(bb, 11, 0);
+//         ==== Group start tag ====
+        int gstStartPos = bb.position();
+        int gstSizePos = putStartTag(bb, 15, 2, 6);
 
-//        putAttribute(bb, 12, -1, VALUE_TYPE_FLOAT, Float.floatToRawIntBits(0.5f));
-//        putAttribute(bb, 13, -1, VALUE_TYPE_FLOAT, Float.floatToRawIntBits(0.5f));
+        putAttribute(bb, 8, -1, VALUE_TYPE_FLOAT, Float.floatToRawIntBits(translateX));
+        putAttribute(bb, 9, -1, VALUE_TYPE_FLOAT, Float.floatToRawIntBits(translateY));
 
-//        posBefore = bb.position();
-//        bb.putInt(gstSizePos, bb.position() - gstStartPos);
-//        bb.position(posBefore);
+        posBefore = bb.position();
+        bb.putInt(gstSizePos, bb.position() - gstStartPos);
+        bb.position(posBefore);
 
         for (int i = 0; i < paths.size(); i++) {
             // ==== Path start tag ====
             int pstStartPos = bb.position();
-            int pstSizePos = putStartTag(bb, 8, (stroke) ? 4: 2);
+            int pstSizePos = putStartTag(bb, 13, stroke ? 4 : 2, 7);
+
 
             // android:fillColor="#aarrggbb", value type: #rgb.
-            putAttribute(bb, 4, -1, VALUE_TYPE_COLOR, paths.get(i).color);
+            putAttribute(bb, 4, -1, VALUE_TYPE_COLOR_F, paths.get(i).color);
 
             // android:pathData="...", value type: string
-            putAttribute(bb, 5, 11 + i, VALUE_TYPE_STRING, 11 + i);
+            // TODO: handle multiple paths
+            putAttribute(bb, 5, 12, VALUE_TYPE_STRING, 12);
 
             if(stroke) {
-                // android:strokeWidth="...", value type: float
-                putAttribute(bb, 6, -1, VALUE_TYPE_FLOAT, Float.floatToRawIntBits(3));
 
                 // android:strokeWidth="...", value type: #rgb
-                putAttribute(bb, 7, -1, VALUE_TYPE_COLOR,  paths.get(i).color);
+                putAttribute(bb, 6, -1, VALUE_TYPE_COLOR_D,  paths.get(i).strokeColor);
+
+                // android:strokeWidth="...", value type: float
+                putAttribute(bb, 7, -1, VALUE_TYPE_FLOAT, Float.floatToRawIntBits(3));
             }
 
             // Write path start tag chunk size
@@ -275,14 +325,16 @@ public class VectorDrawableCreator {
             bb.position(posBefore);
 
             // ==== Path end tag ====
-            putEndTag(bb, 8);
+            putEndTag(bb, 13, 7);
         }
 
         // ==== group end tag ====
-//        putEndTag(bb, 11);
+        putEndTag(bb, 15, 6);
 
         // ==== Vector end tag ====
-        putEndTag(bb, 9);
+        putEndTag(bb, 14, 1);
+
+        putNsEndTag(bb, 11);
 
         // Write XML chunk size
         posBefore = bb.position();
@@ -303,13 +355,13 @@ public class VectorDrawableCreator {
         return binXml;
     }
 
-    private static int putStartTag(ByteBuffer bb, int name, int attributeCount) {
+    private static int putStartTag(ByteBuffer bb, int name, int attributeCount, int lineNumber) {
         // https://justanapplication.wordpress.com/2011/09/25/android-internals-binary-xml-part-six-the-xml-start-element-chunk/
         bb.putShort(CHUNK_TYPE_START_TAG);
         bb.putShort((short) 16);  // Header size
         int sizePos = bb.position();
         bb.putInt(0); // Size, to be set later
-        bb.putInt(0);  // Line number: None
+        bb.putInt(lineNumber);  // Line number: None
         bb.putInt(-1);  // Comment: None
 
         bb.putInt(-1);  // Namespace: None
@@ -324,21 +376,43 @@ public class VectorDrawableCreator {
         return sizePos;
     }
 
-    private static void putEndTag(ByteBuffer bb, int name) {
+    private static void putNsStartTag(ByteBuffer bb, int name) {
+        // https://justanapplication.wordpress.com/2011/09/25/android-internals-binary-xml-part-six-the-xml-start-element-chunk/
+        bb.putShort(CHUNK_TYPE_NS_START_TAG);
+        bb.putShort((short) 16);  // Header size
+        bb.putInt(24); // Size, to be set later
+        bb.putInt(1);  // Line number: None
+        bb.putInt(-1);  // Comment: None
+        bb.putInt(10);  // Namespace: None
+        bb.putInt(name);
+    }
+
+    private static void putEndTag(ByteBuffer bb, int name, int lineNumber) {
         // https://justanapplication.wordpress.com/2011/09/26/android-internals-binary-xml-part-seven-the-xml-end-element-chunk/
         bb.putShort(CHUNK_TYPE_END_TAG);
         bb.putShort((short) 16);  // Header size
         bb.putInt(24);  // Chunk size
-        bb.putInt(0);  // Line number: none
+        bb.putInt(lineNumber);  // Line number: none
         bb.putInt(-1);  // Comment: none
         bb.putInt(-1);  // Namespace: none
+        bb.putInt(name);  // Name: vector
+    }
+
+    private static void putNsEndTag(ByteBuffer bb, int name) {
+        // https://justanapplication.wordpress.com/2011/09/26/android-internals-binary-xml-part-seven-the-xml-end-element-chunk/
+        bb.putShort(CHUNK_TYPE_NS_END_TAG);
+        bb.putShort((short) 16);  // Header size
+        bb.putInt(24);  // Chunk size
+        bb.putInt(1);  // Line number: none
+        bb.putInt(-1);  // Comment: none
+        bb.putInt(10);  // Namespace: none
         bb.putInt(name);  // Name: vector
     }
 
     private static void putAttribute(ByteBuffer bb, int name,
                                      int rawValue, short valueType, int valueData) {
         // https://justanapplication.wordpress.com/2011/09/19/android-internals-resources-part-eight-resource-entries-and-values/#struct_Res_value
-        bb.putInt(10);  // Namespace index in string pool (always the android namespace)
+        bb.putInt(11);  // Namespace index in string pool (always the android namespace)
         bb.putInt(name);
         bb.putInt(rawValue);
         bb.putShort((short) 0x08);  // Value size
@@ -351,14 +425,16 @@ public class VectorDrawableCreator {
 
         public byte[] data;
         public int color;
+        public int strokeColor;
 
-        public PathData(byte[] data, int color) {
+        public PathData(byte[] data, int color, int strokeColor) {
             this.data = data;
             this.color = color;
+            this.strokeColor = strokeColor;
         }
 
-        public PathData(String data, int color) {
-            this(data.getBytes(StandardCharsets.UTF_8), color);
+        public PathData(String data, int color, int strokeColor) {
+            this(data.getBytes(StandardCharsets.UTF_8), color, strokeColor);
         }
 
     }
